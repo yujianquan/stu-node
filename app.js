c046dbf1dddfb15fc8be44b71b38c6f0d739ff48
Yujianquan <1029485996@qq.com>
2019-05-23 16:12:34 +0800

    删除public静态资源文件夹

--- app.js
+++ app.js
@@ -9,16 +9,10 @@ var admin = require('./routes/admin/index')
 
 var app = express();
 
-// view engine setup
-// app.engine('html',require('express-art-template'))
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'html');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/admin',admin);
